b721aaa6527eb86c8c968df3767f754abc916284
Abhinav <abhinavspl2003@gmail.com>
2025-07-12 21:36:48 +0530

    add logic to codebase

---
+++ 04_control_flow/logic.js
@@ -0,0 +1,13 @@
+/* if , if else , else , switch ( if any case matched than it will print 
+    whole console below it except default, so;we have to use break)
+    */
+
+    // every functions executed on the basis of truthness
+
+    // falsly elements ( "",null,undefined, NaN,false,0,-0, BigInt = 0n)
+
+    // truly elements (" ", "0",[],{},function(){})
+    // terniary =>  condition ? true:false      // VVI
+    // ??   // in case of database
+
+    // just take it easy man
